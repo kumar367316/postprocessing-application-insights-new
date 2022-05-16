33379c4255ab1a8b0ac5cb3c931ec922b76b7196
kumar <85148531+kumar367316@users.noreply.github.com>
2022-05-16 17:01:07 +0530

    added insights-new in log

--- src/main/java/com/custom/postprocessing/scheduler/PostProcessingScheduler.java
+++ src/main/java/com/custom/postprocessing/scheduler/PostProcessingScheduler.java
@@ -100,7 +100,7 @@ public class PostProcessingScheduler {
 
 	@Scheduled(cron = PostProcessingConstant.CRONJOB_INTERVAL)
 	public void postProcessing() {
-		logger.info("start postProcessing batch DevOps pipeline deployment testing2");
+		logger.info("start postProcessing batch DevOps pipeline deployment testing:insights-new");
 		String message = smartComPostProcessing();
 		logger.info(message);
 	}
