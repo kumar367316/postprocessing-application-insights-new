4581d29c2e9174c0a5fff619d777ab8834eaae4a
kumar.charanswain <kumarswain367@gmail.com>
2022-05-18 18:45:06 +0530

    test for continous deployment

--- src/main/java/com/custom/postprocessing/scheduler/PostProcessingScheduler.java
+++ src/main/java/com/custom/postprocessing/scheduler/PostProcessingScheduler.java
@@ -97,7 +97,7 @@ public class PostProcessingScheduler {
 
 	@Scheduled(cron = PostProcessingConstant.CRONJOB_INTERVAL)
 	public void postProcessing() {
-		logger.info("insights-new:start postProcessing batch DevOps pipeline deployment testing");
+		logger.info("insights-new continues deployment:start postProcessing batch DevOps pipeline deployment testing");
 		String message = smartComPostProcessing();
 		logger.info(message);
 	}
